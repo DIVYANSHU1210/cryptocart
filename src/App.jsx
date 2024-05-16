a7c12ac81b306200bacf4fb6ec41d68262a077ab
Divyanshu Joshi <divyanshujoshi1012@gmail.com>
2024-05-16 16:58:09 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -5,42 +5,42 @@ function App() {
   const [btc, setBtc] = useState(0);
   const [doge, setDoge] = useState(0);
   const [ripple, setRipple] = useState(0);
-  const[total, setTotal] = useState(0);
 
   const [finalBtc, setFinalBtc] = useState(0);
   const [finalDoge, setFinalDoge] = useState(0);
   const [finalRipple, setFinalRipple] = useState(0);
 
-  const [btcPrice, setBtcPrice] = useState(0);
-  const [dogePrice, setDogePrice] = useState(0);
-  const [ripplePrice, setRipplePrice] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  useEffect(()=>{
-    setTotal((finalBtc + finalDoge + finalRipple).toFixed(2));
-  }, [finalBtc , finalDoge , finalRipple])
+  const btcPrice = 25000;
+  const dogePrice = 0.75;
+  const ripplePrice = 1.5;
+
+  useEffect(() => {
+    const totalAmount =
+      finalBtc * btcPrice + finalDoge * dogePrice + finalRipple * ripplePrice;
+    setTotal(totalAmount.toFixed(2));
+  }, [finalBtc, finalDoge, finalRipple]);
 
   const handleBTC = () => {
     setFinalBtc(finalBtc + btc);
   };
-  
+
   const handleDoge = () => {
     setFinalDoge(finalDoge + doge);
   };
-  
+
   const handleRipple = () => {
     setFinalRipple(finalRipple + ripple);
   };
 
-  const handleAmount = (value, price) => {
-    return (value * price).toFixed(2);
-  };
   return (
     <div>
       <div>
         <div className="btc">
           <Crypto
             name="BTC"
-            price={25000}
+            price={btcPrice}
             value={btc}
             setValueFunc={setBtc}
             clickFunc={handleBTC}
@@ -49,7 +49,7 @@ function App() {
         <div className="doge">
           <Crypto
             name="DOGE"
-            price={0.75}
+            price={dogePrice}
             value={doge}
             setValueFunc={setDoge}
             clickFunc={handleDoge}
@@ -58,7 +58,7 @@ function App() {
         <div className="ripple">
           <Crypto
             name="RIPPLE"
-            price={1.5}
+            price={ripplePrice}
             value={ripple}
             setValueFunc={setRipple}
             clickFunc={handleRipple}
@@ -79,27 +79,23 @@ function App() {
             <tr>
               <td>BTC</td>
               <td>{finalBtc}</td>
-              <td>{handleAmount(finalBtc, 25000)}</td>
+              <td>{(finalBtc * btcPrice).toFixed(2)}</td>
             </tr>
             <tr>
               <td>DOGE</td>
               <td>{finalDoge}</td>
-              <td>{handleAmount(finalDoge, 0.75)}</td>
+              <td>{(finalDoge * dogePrice).toFixed(2)}</td>
             </tr>
             <tr>
               <td>RIPPLE</td>
               <td>{finalRipple}</td>
-              <td>{handleAmount(finalRipple, 1.5)}</td>
+              <td>{(finalRipple * ripplePrice).toFixed(2)}</td>
             </tr>
           </tbody>
           <tfoot>
             <tr>
-              <td colSpan={2}>
-                  TOTAL
-              </td>
-              <td>
-                {total}
-              </td>
+              <td colSpan={2}>TOTAL</td>
+              <td>{total}</td>
             </tr>
           </tfoot>
         </table>
